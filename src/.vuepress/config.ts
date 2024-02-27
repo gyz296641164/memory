4173e503e8408c88de5d187583cc308dbe292e76
gong_yz <296641164@qq.com>
2024-02-28 00:01:43 +0800

    添加评论。

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -14,29 +14,43 @@ export default defineUserConfig({
   },
 
   theme,
-  port: 8099, //自定义项目启动端口号
+  port: 8099, //自定义项目启动端口号  
+
+  //标题层级
+  extendsMarkdownOptions: (markdownOptions, app) => {
+    if (markdownOptions.headers === false) return
+    markdownOptions.headers ??= {}
+    if (markdownOptions.headers.level) return
+    markdownOptions.headers.level = [1, 2, 3, 4]
+  },
 
   plugins: [
+
+    //搜索插件
     searchProPlugin({
       // 索引全部内容
       indexContent: true,
+      hotReload: true,
       // 为分类和标签添加索引
       customFields: [
         {
-          getter: (page) => page.frontmatter.category,
-          formatter: "分类：$content",
+          getter: ({ frontmatter }): string[] => <string[]>frontmatter["tag"],
+          formatter: `Tag: $content`,
         },
         {
-          getter: (page) => page.frontmatter.tag,
-          formatter: "标签：$content",
+          getter: ({ frontmatter }): string[] => <string[]>frontmatter["category"],
+          formatter: `Category: $content`,
         },
       ],
+      indexOptions: {
+        tokenize: (text, fieldName) =>
+          fieldName === "id" ? [text] : cut(text, true),
+      },
     }),
   ],
   shouldPrefetch: false,
 });
 
-// 页面标题层级
-const defaultOptions = {
-  level: [1, 2, 3, 4],
-}
+
+
+
--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -97,8 +97,20 @@ export default hopeTheme({
   plugins: {
     blog: true,
 
-    comment: {
+    //版权信息
+    copyright:{
+      author: "gong_yuzhuo",
+      license: "MIT"
+    },
 
+    //评论插件
+    comment: {
+      provider: "Giscus", //评论插件
+      comment: true, //启用评论功能
+      repo: "Giscus/Giscus", //远程仓库
+      repoId: "MDEwOlJlcG9zaXRvcnkzNTE5NTgwNTM=", //对应自己的仓库Id
+      category: "Announcements",
+      categoryId: "DIC_kwDOFPp0Jc4CcjOR" //对应自己的分类Id
     },
     
     photoSwipe: true,
@@ -177,62 +189,5 @@ export default hopeTheme({
         ],
       }
     }
-
-    // uncomment these if you want a PWA
-    // pwa: {
-    //   favicon: "/favicon.ico",
-    //   cacheHTML: true,
-    //   cachePic: true,
-    //   appendBase: true,
-    //   apple: {
-    //     icon: "/assets/icon/apple-icon-152.png",
-    //     statusBarColor: "black",
-    //   },
-    //   msTile: {
-    //     image: "/assets/icon/ms-icon-144.png",
-    //     color: "#ffffff",
-    //   },
-    //   manifest: {
-    //     icons: [
-    //       {
-    //         src: "/assets/icon/chrome-mask-512.png",
-    //         sizes: "512x512",
-    //         purpose: "maskable",
-    //         type: "image/png",
-    //       },
-    //       {
-    //         src: "/assets/icon/chrome-mask-192.png",
-    //         sizes: "192x192",
-    //         purpose: "maskable",
-    //         type: "image/png",
-    //       },
-    //       {
-    //         src: "/assets/icon/chrome-512.png",
-    //         sizes: "512x512",
-    //         type: "image/png",
-    //       },
-    //       {
-    //         src: "/assets/icon/chrome-192.png",
-    //         sizes: "192x192",
-    //         type: "image/png",
-    //       },
-    //     ],
-    //     shortcuts: [
-    //       {
-    //         name: "Demo",
-    //         short_name: "Demo",
-    //         url: "/demo/",
-    //         icons: [
-    //           {
-    //             src: "/assets/icon/guide-maskable.png",
-    //             sizes: "192x192",
-    //             purpose: "maskable",
-    //             type: "image/png",
-    //           },
-    //         ],
-    //       },
-    //     ],
-    //   },
-    // },
   },
 });
