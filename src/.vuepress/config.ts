b9ff19ca954b92b7fa079e62a205b8001a312983
gong_yz <296641164@qq.com>
2024-03-05 12:18:20 +0800

    添加百度统计。

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -1,11 +1,11 @@
 import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 import { searchProPlugin } from "vuepress-plugin-search-pro";
-import MarkdownIt from 'markdown-it';
-import { headersPlugin } from '@mdit-vue/plugin-headers';
-import type { MarkdownItEnv } from '@mdit-vue/types';
+import commonjs from '@rollup/plugin-commonjs';
+
 
 export default defineUserConfig({
+
   base: '/',
   locales: {
     "/": {
@@ -19,7 +19,24 @@ export default defineUserConfig({
 
   port: 8099, //自定义项目启动端口号  
 
+  head: [
+    // 添加百度统计
+    [
+        'script', {}, `
+        var _hmt = _hmt || [];
+        (function() {
+          var hm = document.createElement("script");
+          hm.src = "https://hm.baidu.com/hm.js?6cb4b3bacbe228f6c0c1476ee3382b64";
+          var s = document.getElementsByTagName("script")[0]; 
+          s.parentNode.insertBefore(hm, s);
+        })();
+        `
+    ],
+  ],
+
   plugins: [
+    //解决打包问题
+    commonjs() as any, // 要放在第一行，否则不生效
 
     //搜索插件
     searchProPlugin({
@@ -37,10 +54,6 @@ export default defineUserConfig({
           formatter: `Category: $content`,
         },
       ],
-      indexOptions: {
-        tokenize: (text, fieldName) =>
-          fieldName === "id" ? [text] : cut(text, true),
-      },
     }),
   ],
   shouldPrefetch: false,
